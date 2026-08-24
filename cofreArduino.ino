68414b214ee0ed5a4191b9e5a23f6fc58e2b0867
Tomas Tonial <tomasctonial@gmail.com>
2026-08-24 20:06:42 -0300

    Refactor password logic and input handling
    Refactor password handling to use an array and improve logic for selecting values based on potentiometer input.

--- cofreArduino.ino
+++ cofreArduino.ino
@@ -1,12 +1,15 @@
 int luzinha = 9;
 int potenciometro = A0;
-int senha = 4;
+int senha[] = {1, 2, 3, 4};
 int valorEscolhido;
 int estadoBotao;
 int statusLuz = 0;
 int valorP = 0;
 int botao = 12;
 int buzina = 11;
+
+int posicaoSenha = 0;
+int senhaDigitada[4];
 
 void setup() {
   pinMode(luzinha, OUTPUT);
@@ -20,47 +23,52 @@ void loop() {
   estadoBotao = digitalRead(botao);
   valorP = analogRead(potenciometro);
   valorP = map(valorP, 0, 1023, 0, 255);
-  if (valorP <= 51){
-    valorEscolhido = 0;
+
+  if (valorP <= 51) valorEscolhido = 0;
+  else if (valorP <= 102) valorEscolhido = 1;
+  else if (valorP <= 153) valorEscolhido = 2;
+  else if (valorP <= 204) valorEscolhido = 3;
+  else valorEscolhido = 4;
+
+  if (estadoBotao == HIGH) {
     compararResultado();
+    delay(100);
   }
-  if (valorP > 51 && valorP <= 102){
-    valorEscolhido = 1;
-    compararResultado();
-  }
-  if (valorP > 102 && valorP <= 153){
-    valorEscolhido = 2;
-    compararResultado();
-  }
-  if (valorP > 153 && valorP <= 204){
-    valorEscolhido = 3;
-    compararResultado();
-  }
-  if (valorP > 204 && valorP <= 255){
-    valorEscolhido = 4;
-    compararResultado();
-  }
+
+  Serial.print("Selecionado: ");
   Serial.println(valorEscolhido);
-  if (valorEscolhido != senha){
-    digitalWrite(luzinha, LOW);
-    statusLuz = 0;
-  }
+  delay(300);
 }
 
-void compararResultado(){
-  if(estadoBotao == HIGH){
-    if (valorEscolhido == senha){
-      if(statusLuz == 0){
-        digitalWrite(luzinha, HIGH);
-        statusLuz = 1;
+void compararResultado() {
+  if (posicaoSenha < 4) {
+    senhaDigitada[posicaoSenha] = valorEscolhido;
+    posicaoSenha++;
+    
+    digitalWrite(buzina, HIGH);
+    delay(50);
+    digitalWrite(buzina, LOW);
+  }
+
+  if (posicaoSenha == 4) {
+    bool senhaCorreta = true;
+
+    for (int i = 0; i < 4; i++) {
+      if (senhaDigitada[i] != senha[i]) {
+        senhaCorreta = false;
       }
     }
-    else{
+
+    if (senhaCorreta) {
+      digitalWrite(luzinha, HIGH);
+      statusLuz = 1;
+    } else {
       digitalWrite(luzinha, LOW);
-      statusLuz = 0;
       digitalWrite(buzina, HIGH);
-      delay(100);
+      delay(1000);
       digitalWrite(buzina, LOW);
     }
+
+    posicaoSenha = 0;
   }
 }
